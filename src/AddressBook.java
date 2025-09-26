f8c647a91dfb811cee4ac6e6e984a9f88da894bd
zaineb <zainebbenhmida@cmail.carleton.ca>
2025-09-25 20:23:48 -0400

    AddressBook

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,32 +1,44 @@
 import java.util.ArrayList;
 
 public class AddressBook {
-    private final ArrayList<BuddyInfo> buddies;
+    private ArrayList<BuddyInfo> myBuddies;
 
     public AddressBook() {
-        this.buddies = new ArrayList<>();
+        myBuddies = new ArrayList<>();
     }
 
-    public void addBuddy(BuddyInfo buddy){
-        buddies.add(buddy);
+    public void addBuddy(BuddyInfo aBuddy){
+        if(aBuddy != null) {
+            myBuddies.add(aBuddy);
+        }
     }
 
-    public void removeBuddy(BuddyInfo buddy){
-        buddies.remove(buddy);
+    public BuddyInfo removeBuddy(int index){
+        if(index >= 0 && index < myBuddies.size()){
+            return myBuddies.remove(index);
+
+        }
+        return null;
+
     }
 
     public static void main(String[] args){
-        AddressBook book = new AddressBook();
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(0);
 
+        /*
         BuddyInfo buddy1 = new BuddyInfo("buddy1");
         BuddyInfo buddy2 = new BuddyInfo("buddy2");
 
-        book.addBuddy(buddy1);
-        book.addBuddy(buddy2);
-        book.removeBuddy(buddy2);
+        addressBook.addBuddy(buddy1);
+        addressBook.addBuddy(buddy2);
+        addressBook.removeBuddy(buddy2);
 
-        System.out.println(book.buddies);
+        System.out.println(addressBook.buddies);
         System.out.println("AddressBook");
+         */
     }
 }
 
