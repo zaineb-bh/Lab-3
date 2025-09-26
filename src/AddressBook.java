e3a9a643c2b73aebf8da223260ced02d084a3e88
zaineb <zainebbenhmida@cmail.carleton.ca>
2025-09-25 22:59:44 -0400

    added new method getBuddies

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -19,17 +19,24 @@ public class AddressBook {
         }
 
     }
+    public ArrayList<BuddyInfo> getBuddies(){
+        return myBuddies;
+    }
+
 
     public static void main(String[] args){
         BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(0);
+        ArrayList<BuddyInfo> buddiesList = addressBook.getBuddies();
 
         System.out.println(addressBook.myBuddies);
         System.out.println("AddressBook");
+        System.out.println(buddiesList);
         //a change
         //another change, made in GitHub website
+        //a change made in the new branch
 
     }
 }
